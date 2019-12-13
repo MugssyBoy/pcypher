66047878d91338cd2c578cb3664823ff67bc2f91
alex <43148847+MugssyBoy@users.noreply.github.com>
2019-12-13 10:33:43 +0800

    updated pcypher.js

--- src/pcypher.js
+++ src/pcypher.js
@@ -1,8 +1,8 @@
 const key = require('ckey');
 const crypto = require('crypto');
 
-module.exports = {
-    pcypher: password => new Promise((resolve, reject) => crypto.scrypt(password, key.SECRET_KEY, parseInt(key.KEY_LENGTH), (err, dd) => resolve(dd.toString('hex')))),
-    hasher: async password => await pcypher(password),
+module.exports.pcypher = {
+    encrypt: password => new Promise((resolve, reject) => crypto.scrypt(password, key.SECRET_KEY, parseInt(key.KEY_LENGTH), (err, dd) => resolve(dd.toString('hex')))),
+    hasher: async password => await encrypt(password),
     comparePassword: async (plainTextPassword, hashedPassword) => await hasher(plainTextPassword) === hashedPassword ? true : false
 };
