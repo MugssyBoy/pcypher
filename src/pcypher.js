0bda1620c500c9a8fd8bca4d115aff6852a86d7e
alex <43148847+MugssyBoy@users.noreply.github.com>
2020-01-21 20:05:43 +0800

    updated pcypher.js

--- src/pcypher.js
+++ src/pcypher.js
@@ -3,6 +3,10 @@ const crypto = require('crypto');
 
 const encrypt = password => new Promise((resolve, reject) => crypto.scrypt(password, key.SECRET_KEY, parseInt(key.KEY_LENGTH), (err, dd) => resolve(dd.toString('hex'))));
 
-exports.hasher =  async password => await encrypt(password);
-exports.comparePassword = async (plainTextPassword, hashedPassword) => await hasher(plainTextPassword) === hashedPassword ? true : false;
-exports.generateToken = async () => await crypto.randomBytes(parseInt(key.KEY_LENGTH)).toString('hex');
+const hasher =  async password => await encrypt(password);
+const comparePassword = async (plainTextPassword, hashedPassword) => await hasher(plainTextPassword) === hashedPassword ? true : false;
+const generateToken = async () => await crypto.randomBytes(parseInt(key.KEY_LENGTH)).toString('hex');
+
+module.exports = {
+    hasher, comparePassword, generateToken
+};
